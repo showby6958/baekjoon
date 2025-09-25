5b89103cebfc4f50953a4c81c0c3ef54a6b4b263
Jinho Jang <wkdwlsgh9622@gmail.com>
2025-09-25 17:54:51 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+res = []
+
+def dfs():
+    if len(res) == m:
+        print(' '.join(map(str, res)))
+        return
+
+    for i in range(1, n+1):
+        if i not in res:
+            res.append(i)
+            dfs()
+            res.pop()
+
+dfs()
